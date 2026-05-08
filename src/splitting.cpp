68b62b25f1d421d17060f7a9e1b26f08186f59ec
JoseSantos56 <josedrmsantos1@gmail.com>
2026-05-08 21:04:36 +0100

    ChooseWeb done

--- src/splitting.cpp
+++ src/splitting.cpp
@@ -1,1 +1,22 @@
 #include "Graph.h"
+#include "splitting.h"
+
+#include <vector>
+
+// Vou ter de dar include ao coloring.h
+
+int chooseWeb(Graph<int>& g) {          // Greedy
+    int density = -1;
+    int resWeb = -1;
+
+    for (auto w: g.getVertexSet()) {        // A web com maior densidade é escolhida
+        int indegree = w->getIndegree();
+
+        if (indegree > density) {
+            density = indegree;
+            resWeb = w->getInfo();
+        }
+    }
+
+    return resWeb;
+}
--- src/splitting.h
+++ src/splitting.h
@@ -1,7 +1,9 @@
 #ifndef SPLITTING_H
 #define SPLITTING_H
+#include "Graph.h"
+#include <utility>
 
-
-
+int chooseWeb(Graph<int>& g);       // Greedy escolher a web que têm amior innterseção de webs
+std::pair<int,int> splitWeb();      // Splittar nos intervalos onde a variavel não é usado o maior tempo possivel
 
 #endif //SPLITTING_H
