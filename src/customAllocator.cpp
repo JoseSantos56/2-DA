c5d422571edae5ce6d44fea72ad9d5c188ba4b0c
JoseSantos56 <josedrmsantos1@gmail.com>
2026-05-16 22:15:06 +0100

    backtrackDSatur, initState and greedyDSatur implemented

--- src/customAllocator.cpp
+++ src/customAllocator.cpp
@@ -48,8 +48,122 @@ void updateSaturation(Graph<int>& g, ColoringState& state, const int webId, cons
     }
 }
 
+bool backtrackDSatur(Graph<int>& g, const int k, ColoringState& state, int spillsLeft) {
 
+    if (state.unprocessed.empty()) return true;
 
+    int webId = chooseNextWeb(g, state);
+    if (webId == -1) return false;
+
+    for (int color = 0; color < k; color++) {
+
+        if (isColorAvailable(state, webId, color)) {
+
+            // Guardar o estado inicial
+            std::vector<int> oldColors = state.colors;
+            std::vector<bool> oldSpilled = state.spilled;
+            std::vector<bool> oldProcessed = state.processed;
+            std::vector<std::set<bool>> oldAdjColors = state.adjColors;
+            std::vector<int> oldSaturation = state.saturation;
+            std::set<bool> oldUnprocessed = state.unprocessed;
+
+            // Aplicar a cor e o que isso implica
+            state.colors[webId] = color;
+            state.processed[webId] = true;
+            state.unprocessed.erase(webId);
+            updateSaturation(g, state, webId, color);
+
+            if (backtrackDSatur(g, k, state, spillsLeft)) return true;
+
+            // Backtrack
+            std::vector<int> colors = oldColors;
+            std::vector<bool> spilled = oldSpilled;
+            std::vector<bool> processed = oldProcessed;
+            std::vector<std::set<bool>> adjColors = oldAdjColors;
+            std::vector<int> saturation = oldSaturation;
+            std::set<bool> unprocessed = oldUnprocessed;
+        }
+    }
+
+    // Tentar dar spill para ver se resolve o problema
+    if (spillsLeft > 0 ) {
+
+        std::vector<bool> oldSpilled = state.spilled;
+        std::vector<bool> oldProcessed = state.processed;
+        std::set<bool> oldUnprocessed = state.unprocessed;
+
+        // Alterar para spilled e tudo o que isso implica
+        state.spilled[webId] = true;;
+        state.processed[webId] = true;
+        state.unprocessed.erase(webId);
+
+        if (backtrackDSatur(g, k, state, spillsLeft - 1)) return true;
+
+        // Backtrack
+        state.spilled = oldSpilled;
+        state.processed = oldProcessed;
+        state.unprocessed = oldUnprocessed;
+    }
+
+    return false;
+}
+
+ColoringState initState(Graph<int>& g, int v) {
+
+    ColoringState state;
+    state.colors.resize(v, -1);
+    state.spilled.resize(v, false);
+    state.processed.resize(v, false);
+    state.saturation.resize(v, 0);
+    state.adjColors.resize(v);
+
+    for (auto vertex : g.getVertexSet()) {
+        state.unprocessed.insert(vertex->getInfo());
+    }
+
+    return state;
+}
+
+bool greedyDSatur(Graph<int>& g, int k, std::vector<int>& colors, std::vector<bool>& spilled) {
+
+    int v = g.getNumVertex();
+
+    ColoringState state = initState(g, v);
+
+    while (!state.unprocessed.empty()) {
+
+        int webId = chooseNextWeb(g, state);
+        if (webId == -1) return false;
+
+        bool assigned = false;
+
+        for (int color = 0; color < k; color++) {
+
+            if (isColorAvailable(state, webId, color)) {
+
+                state.colors[webId] = color;
+                state.processed[webId] = true;
+                state.unprocessed.erase(webId);
+                updateSaturation(g, state, webId, color);
+
+                assigned = true;
+                break;
+            }
+        }
+
+        if (!assigned) {
+            // Dá spill caso não consiga colorir
+            state.spilled[webId] = true;
+            state.processed[webId] = true;
+            state.unprocessed.erase(webId);
+        }
+    }
+
+    colors = state.colors;
+    spilled = state.spilled;
+
+    return true;
+}
 
 
 
--- src/customAllocator.h
+++ src/customAllocator.h
@@ -17,7 +17,8 @@ struct ColoringState {
 int chooseNextWeb(Graph<int>& g, const ColoringState& state);       // Com base em DSatur usando o vertice de maior saturação
 bool isColorAvailable(ColoringState& state, const int webId, const int color);
 void updateSaturation(Graph<int>& g, ColoringState& state, const int webId, const int color);         // Update da saturação dos vizinhos do vértice
-bool backtrackDSatur(Graph<int>& g, int k, ColoringState& state);           // Algoritmo DSatur + backtracking (Escolher o vértice com maior saturação, combinando um aboradegem de backtracking)
+bool backtrackDSatur(Graph<int>& g, const int k, ColoringState& state, int spillsLeft);           // Algoritmo DSatur + backtracking (Escolher o vértice com maior saturação, combinando um aboradegem de backtracking)
+ColoringState initState(Graph<int>& g, int v);              // Incialização do parametro state
 bool greedyDSatur(Graph<int>& g, int k, std::vector<int>& colors, std::vector<bool>& spilled);          // Abordagem hibrida greedy com decisão baseada em DSatur
 bool customAllocate(Graph<int>& g, int k, std::vector<int>& colors, std::vector<bool>& spilled);        // Algoritmo final junta todas as funções
 
